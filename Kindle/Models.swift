1f273dda676f64200447fceab61b447326b7173f
Set Kyar Wa Lar <setkyar16@gmail.com>
2018-11-07 23:21:58 +0800

    setup navigation, ui table view and remove story board

---
+++ Kindle/Models.swift
@@ -0,0 +1,31 @@
+//
+//  Models.swift
+//  Kindle
+//
+//  Created by Macbook on 11/7/18.
+//  Copyright © 2018 Set Kyar Wa Lar. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    let title: String
+    let author: String
+    let pages: [Page]
+    
+    init(title: String, author: String, pages: [Page]){
+        self.title = title
+        self.author = author
+        self.pages = pages
+    }
+}
+
+class Page {
+    let number: Int
+    let text: String
+    
+    init(number: Int, text: String) {
+        self.number = number
+        self.text = text
+    }
+}
--- Kindle/ViewController.swift
+++ Kindle/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var books: [Book]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Kindle"
+        
+        view.backgroundColor = .red
+        //can provide custom code staring here.
+        
+        setupBooks()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupBooks(){
+        let page1 = Page(number: 1, text: "Text for the frist page")
+        let page2 = Page(number: 2, text: "Text for the second two")
+        
+        let pages = [page1, page2]
+        
+        let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: pages)
+        
+        let book2 = Book(title: "Bill Gates: A Biography", author: "Michael Becraft", pages: [
+            Page(number: 1, text: "Text for page 3"),
+            Page(number: 2, text: "Text for page 4"),
+            Page(number: 3, text: "Text for page 5"),
+            Page(number: 4, text: "Text for page 6")
+            ])
+        
+        self.books = [book, book2]
+        
+        guard let books = self.books else {return}
+        
+        for book in self.books! {
+            print(book.title)
+            
+            for page in book.pages {
+                print(page.text)
+            }
+        }
     }
-
 
 }
-
